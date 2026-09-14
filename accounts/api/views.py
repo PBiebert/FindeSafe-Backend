7b245984d1d2d00fd0042d16d862f6ca82bd0d99
Philipp Biebert <philipp.biebert@gmail.com>
2026-09-14 17:00:42 +0200

    feat: Implement registration endpoint and update settings for environment variables

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,11 @@
+from rest_framework import status
+from rest_framework.authtoken.views import Response
 from rest_framework.views import APIView
 
 
 class RegisterView(APIView):
-
-    pass
+    def post(self, request):
+        print(request.data)
+        return Response(
+            {"message": "Registration successful."}, status=status.HTTP_201_CREATED
+        )
--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9g20w)9y510ofluzfqe@)yc2v#wnq&oj*i&#8hg%@w+j2cb6$%"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -38,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "accounts.apps.accounts",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
